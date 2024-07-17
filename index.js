5ccf2a349026d1c8545a5cf5d47d35192f6e27a9
emmanuel ronoh <eronoh036@gmail.com>
2024-07-17 15:25:25 +0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+// Function to divide two numbers
+const divide = (a, b) => {
+    if (b === 0) {
+        throw new Error('Division by zero is not allowed');
+    }
+    return a / b;
+};
 
-let divide = 2000/100;
+// Arrow function to square a number
+const square = x => x * x;
 
-let square = x => x * x;
+// Arrow function to add two numbers
+const add = (a, b) => a + b;
 
-let add = (a, b) => a + b; 
+// Export the functions to be accessible from outside
+module.exports = {
+    divide,
+    square,
+    add
+};
